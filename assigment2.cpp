f9e9f7260bcd02386b6207d0a62513ac7906f8c8
Kyrylo Shykhalov <kshykhalov@kse.org.ua>
2024-09-24 09:20:16 +0300

    initial commit

---
+++ assigment2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+class Figure {
+private:
+     virtual void draw_the_figure();
+};
+
+class Circle {
+private:
+     int radius_circle;
+     int area;
+public: Figure   {
+     void draw();
+     }
+};
+
+int main ()
+{
+     cout << "Hello in draw studio ";
+
+}
