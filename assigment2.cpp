308ce946fa358aa41330e48b51c1f37c2561a788
Kyrylo Shykhalov <kshykhalov@kse.org.ua>
2024-09-30 15:52:07 +0300

    first commit with black board class
    create black board class, in it create constructor and destructor

--- assigment2.cpp
+++ assigment2.cpp
@@ -3,11 +3,34 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
-#include <algorithm>
-#include <map>
+#include <memory>
+#include "Shape.h"
 using namespace std;
 
+
+class BlackBoard {
+private:
+     vector<std::unique_ptr<Shape>> shapes;
+     vector<std::vector<char>> grid;
+     const int BOARD_WIDTH = 80;
+     const int BOARD_HEIGHT = 25;
+public:
+     BlackBoard() : grid(BOARD_HEIGHT, std::vector<char>(BOARD_WIDTH, ' ')) {}
+
+     void print() {
+          for (auto& row : grid) {
+               for (char c : row) {
+                    std::cout << c;
+               }
+               std::cout << "\n";
+          }
+     }
+
+};
+
 class Figure {
+     string circle;
+     string cube;
 private:
      virtual void draw_the_figure();
 };
@@ -16,13 +39,15 @@ class Circle {
 private:
      int radius_circle;
      int area;
-public: Figure   {
-     void draw();
+public: Figure  {
+     void Figure(int r, int area): int radius_circle(r),
      }
 };
 
 int main ()
 {
+     Blackboard bord;
      cout << "Hello in draw studio ";
-
+     cout << "Write a Height of a figure" ;
+     cin
 }
