5e92f781dbf08aa4c20d66754a40337e6d32fd5f
Kyrylo Shykhalov <kshykhalov@kse.org.ua>
2024-10-02 15:04:20 +0300

    commit
    rewrite main

--- assigment2.cpp
+++ assigment2.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-class Figure abstract {
+class Figure {
 protected:
     int x;
     int y;
@@ -23,21 +23,21 @@ class Circle: public Figure {
 protected:
     int radius_circle;
     int height;
+    int x;
+    int y;
 public:
-    Circle( int radius, int x_cordinate, int y_cordinate, int height ) : radius_circle(radius),
-    x(x_cordinate), x(y_cordinate), height(height) {}
     void draw(std::vector<std::vector<char>> &grid) override {
-        int height = grid.size();
-        int width = grid[0].size();
-        for (int i =0; i << height; i++ ) {
-            for (int j = 0; j < width; ++j) {
+        int grid_height = grid.size();
+        int grid_width = grid[0].size();
+        for (int i = 0; i < grid_height; i++) {
+            for (int j = 0; j < grid_width; ++j) {
                 double dist = sqrt(pow(j - x, 2) + pow(i - y, 2));
                 if (abs(dist - radius_circle) < 0.5) {
                     grid[i][j] = 'O';
+                }
+            }
         }
     }
-
-
 };
 
 class Rectangle: public Figure  {
@@ -45,6 +45,9 @@ protected:
     int width;
     int height;
 public:
+    void draw_rectangle (int w, int h) {
+        
+    }
 
 };
 class Line: public Figure {
@@ -150,36 +153,92 @@ int main ()
 {
      Blackboard board;
      string command;
-     while (true) {
+     while(true) {
           cin >> command;
-          if (command == "draw") {
+          if(command == "draw") {
                board.print();
-          } else if (command == "add") {
-               std::string shapeType;
-               std::cin >> shapeType;
-               if (shapeType == "circle") {
+          }
+          else if(command == "add") {
+               string shapeType;
+               cin >> shapeType;
+               if(shapeType == "circle") {
                     int x, y, radius;
-                    std::cin >> x >> y >> radius;
-
-               } else if (shapeType == "rectangle") {
-                    int x, y, width, height;
-                    std::cin >> x >> y >> width >> height;
-
+                    cin >> x >> y >> radius;
+                    if(cin.fail() || radius <= 0) {
+                        cout << "Invalid parameters for Circle. Usage: add circle x y radius\n";
+                        cin.clear();
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                        continue;
+                    }
+                    board.addFigure(make_unique<Circle>(x, y, radius));
                }
-          } else if (command == "undo") {
-
-          } else if (command == "clear") {
+               else if(shapeType == "rectangle") {
+                    int x, y, width, height;
+                    cin >> x >> y >> width >> height;
+                    if(cin.fail() || width <= 0 || height <= 0) {
+                        cout << "Invalid parameters for Rectangle. Usage: add rectangle x y width height\n";
+                        cin.clear();
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                        continue;
+                    }
+                    board.addFigure(make_unique<Rectangle>(x, y, width, height));
+               }
+               else if(shapeType == "line") {
+                    int x, y, length;
+                    char dir;
+                    cin >> x >> y >> length >> dir;
+                    if(cin.fail() || length <= 0 || (dir != 'H' && dir != 'V')) {
+                        cout << "Invalid parameters for Line. Usage: add line x y length direction(H/V)\n";
+                        cin.clear();
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                        continue;
+                    }
+                    board.addFigure(make_unique<Line>(x, y, length, dir));
+               }
+               else if(shapeType == "triangle") {
+                    int x, y, height;
+                    cin >> x >> y >> height;
+                    if(cin.fail() || height <= 0) {
+                        cout << "Invalid parameters for Triangle. Usage: add triangle x y height\n";
+                        cin.clear();
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                        continue;
+                    }
+                    board.addFigure(make_unique<Triangle>(x, y, height));
+               }
+               else {
+                    cout << "Unknown shape type: " << shapeType << ". Type 'shapes' to see available shapes.\n";
+               }
+          }
+          else if(command == "undo") {
+               board.removeLastFigure();
+          }
+          else if(command == "clear") {
                board.clear();
-          } else if (command == "save") {
-               std::string filePath;
-               std::cin >> filePath;
+               cout << "Blackboard cleared.\n";
+          }
+          else if(command == "save") {
+               string filePath;
+               cin >> filePath;
                board.save(filePath);
-          } else if (command == "load") {
-               std::string filePath;
-               std::cin >> filePath;
+          }
+          else if(command == "load") {
+               string filePath;
+               cin >> filePath;
                board.load(filePath);
-          } else if (command == "exit") {
+          }
+          else if(command == "shapes") {
+               board.listAvailableShapes();
+          }
+          else if(command == "list") {
+               board.listFigures();
+          }
+          else if(command == "exit") {
+               cout << "Exiting the application. Goodbye!\n";
                break;
+          }
+          else {
+               cout << "Unknown command. Type 'help' to see available commands.\n";
           }
      }
 
